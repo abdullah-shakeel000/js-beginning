af6e8ad7cf6f3690ca3d0dc733c0e82dde7159d5
abdullah-shakeel000 <151764677+abdullah-shakeel000@users.noreply.github.com>
2024-09-09 20:14:38 +0000

    container config

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,9 +2,10 @@ const account_id = 12323
 let account_email = "abdul;ahshakeel30008@gmail.com"
 var account_pass = "2222"
 account_city = "Rawalpindi"
-
+let account_status
+// prefer not to use var
 //account_id = 2 // not allowed
 account_email = "nigga123@gmail.com"
 account_pass = "33411"
 console.log(account_id);
-console.table([account_id, account_email, account_pass, account_city])
+console.table([account_id, account_email, account_pass, account_city, account_status])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3)// we r using node.js not brower
+
+console.log(3+3)
+console.log("Abdullah");
+// code readability should be high
+
+let name ="Me"
+let age = 18
+let isloggedin = false
+
+//number =>2 to power of 53
+//bigint for greater no of range
+//string => ""
+//boolean => true/false
+//null
+//undefined => standalone value and undefined value
+//symbol => if we wanna talk about uniques the we wuse symbol
+
+//object
+console.log(typeof null ); // gives output 'object'
+console.log(typeof undefind) // gives output 'undefind'
