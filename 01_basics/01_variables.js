b47001894ec5ba57f510e5e48d0c1370d3dfe501
abdullah-shakeel000 <151764677+abdullah-shakeel000@users.noreply.github.com>
2024-09-09 19:41:55 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const account_id = 12323
+let account_email = "abdul;ahshakeel30008@gmail.com"
+var account_pass = "2222"
+account_city = "Rawalpindi"
+
+//account_id = 2 // not allowed
+account_email = "nigga123@gmail.com"
+account_pass = "33411"
+console.log(account_id);
+console.table([account_id, account_email, account_pass, account_city])
