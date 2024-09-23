6fffc7e59a5700886a9a6b9bf25f5bc037bf887c
abdullah-shakeel000 <151764677+abdullah-shakeel000@users.noreply.github.com>
2024-09-23 20:25:11 +0000

    con

---
+++ 01_basics/03_convertionoperations.js
@@ -0,0 +1,26 @@
+//Convertion into number:
+//let score = null
+//console.log(typeof score);
+//console.log(typeof(score));
+//let valueInNumber = Number(score)
+///console.log(valueInNumber);
+
+//"33" gives 33
+//but "33abc" gives NaN
+// true gives 1 and false gives 0
+//underfined gives NaN
+//null gives 0
+
+//Convertion into Boolean:
+//let  isloggedIn = "Me"
+//let booleanisloggedIn = //Boolean(isloggedIn)
+//console.log(booleanisloggedIn);
+
+// 1 gives true, 0 gives false
+//"" gives false
+//"Me" gives true
+
+//Convertion into string
+let someNumber = 23
+//let stringNumber = String(someNumber)
+console.log(stringNumber);
